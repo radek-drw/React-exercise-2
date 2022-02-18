94e55e619b8711070e829f1b3916a10d59135e4f
Radek Drweski <rdrweski@gmail.com>
2022-02-18 07:21:57 +0000

    code tidy up

--- ListItems.js
+++ ListItems.js
@@ -1,20 +1,18 @@
-const ListItems = props => {
-   const items = props.items.map(item => {
-      return (
-         <Item
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            active={item.active}
-            price={item.price}
-            changeStatus={props.changeStatus}
-         />
-      )
-   })
+const ListItems = ({ changeStatus, items }) => {
+   const itemsList = items.map(item => (
+      <Item
+         key={item.id}
+         id={item.id}
+         name={item.name}
+         active={item.active}
+         price={item.price}
+         changeStatus={changeStatus}
+      />
+   ))
 
    return (
       <ul>
-         {items}
+         {itemsList}
       </ul>
    )
 }
